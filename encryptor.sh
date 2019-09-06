939c9f1a87234aac3fe0eb04447aacfde84ae38a
Charles Edward Pax <charles.pax@gmail.com>
2019-09-06 00:06:09 -0400

    Adding getopts support. Can display a qr code in terminal.

--- encryptor.sh
+++ encryptor.sh
@@ -1,17 +1,55 @@
 #!/bin/bash
 
-IN_FILE=$1
-GPG_FILE="$IN_FILE.gpg"
-OUT_FILE="$IN_FILE.out.sh"
+print_usage() {
+    echo "encryptor -f <input filename> -o <output filename."
+    echo "    h - print this help message"
+    echo "    i - input filename"
+    # TODO support output file name
+    #echo "    o - output filename"
+    echo "    q - print qr code if qrencode is installed"
+}
 
-gpg -o $GPG_FILE -c $IN_FILE
+encrypt() {
+    # TODO Spilt this into two functions:
+    #  1. generate encrypted string
+    #  2. Create the output file
+    GPG_FILE="$IN_FILE.gpg"
+    OUT_FILE="$IN_FILE.out.sh"
 
-echo "#!/bin/bash" > $OUT_FILE
-echo >> $OUT_FILE
-echo -n "echo " >> $OUT_FILE
-base64 --wrap=0 $GPG_FILE >> $OUT_FILE
-echo " | base64 -d | gpg -o out.txt" >> $OUT_FILE
+    gpg -o $GPG_FILE -c $IN_FILE
 
-# Clean up
-rm $GPG_FILE
+    echo "#!/bin/bash" > $OUT_FILE
+    echo >> $OUT_FILE
+    echo -n "echo " >> $OUT_FILE
+    base64 --wrap=0 $GPG_FILE >> $OUT_FILE
+    echo " | base64 -d | gpg -o out.txt" >> $OUT_FILE
 
+    # Clean up
+    rm $GPG_FILE
+}
+
+print_qrcode() {
+    cat $OUT_FILE | qrencode -s 1 -t ansiutf8
+}
+
+while getopts 'hi:q' flag; do
+  case "${flag}" in
+    h) print_usage
+       exit 1 ;;
+    i) IN_FILE="${OPTARG}" ;;
+    q) q_flag='true' ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+# TODO execute this only if the input file is given
+# TODO if no output file is given, output text to terminal
+encrypt
+
+# TODO Do not display qrcode if encrypt function fails
+# TODO if no output file is given, display only qr code
+if [ "$q_flag" == "true" ]
+then
+    print_qrcode
+fi
